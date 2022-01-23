d33a3c28f50ae7ffd1f66e20e2b55c3c806020c5
Mikhail Melnik <by.zumzoom@gmail.com>
2022-01-23 18:43:46 +0400

    fix comment

--- test/ERC20Farmable.js
+++ test/ERC20Farmable.js
@@ -548,7 +548,7 @@ contract('ERC20Farmable', function ([wallet1, wallet2, wallet3]) {
             await this.token.mint(wallet1, '1');
             await this.token.mint(wallet2, '3');
 
-            // 72000 UDSC per week for 2 weeks
+            // 36000 UDSC per week for 2 weeks
             await this.farm.startFarming('72000', time.duration.weeks(2), { from: wallet1 });
 
             await this.token.farm(this.farm.address, { from: wallet1 });
