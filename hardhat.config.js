1a578128c28b0444dc0acc534a8107bc86541ac4
Mikhail Melnik <by.zumzoom@gmail.com>
2022-03-15 17:03:25 +0400

    bump solc version

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ module.exports = {
         },
     },
     solidity: {
-        version: '0.8.11',
+        version: '0.8.12',
         settings: {
             optimizer: {
                 enabled: true,
--- test/mock/ERC20FarmableMock.sol
+++ test/mock/ERC20FarmableMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../../contracts/ERC20Farmable.sol";
