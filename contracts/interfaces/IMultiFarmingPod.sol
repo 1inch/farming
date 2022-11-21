3b9f392f30469113107453fc03f10b1f0f304c21
Mikhail Melnik <by.zumzoom@gmail.com>
2022-11-21 12:55:11 +0400

    fix typo

--- contracts/interfaces/IMultiFarmingPod.sol
+++ contracts/interfaces/IMultiFarmingPod.sol
@@ -6,13 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@1inch/erc20-pods/contracts/interfaces/IPod.sol";
 
 interface IMultiFarmingPod is IPod {
-    function farmed(IERC20 rawardsToken, address account) external view returns(uint256);
-    function claim(IERC20 rawardsToken) external;
+    function farmed(IERC20 rewardsToken, address account) external view returns(uint256);
+    function claim(IERC20 rewardsToken) external;
     function claim() external;
 
     // only owner functions
     function setDistributor(address distributor_) external;
 
     // only distributor functions
-    function startFarming(IERC20 rawardsToken, uint256 amount, uint256 period) external;
+    function startFarming(IERC20 rewardsToken, uint256 amount, uint256 period) external;
 }
